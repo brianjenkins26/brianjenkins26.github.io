d4e8ca245f629d72dd188a43f5b21d10382bcd0f
brianjenkins26 <brianjenkins26@gmail.com>
2016-04-07 14:15:55 +0000

    first push

---
+++ main.js
@@ -0,0 +1,7 @@
+var menuButton = document.getElementById("menuButton");
+//------------------------
+menuButton.onclick = dropMenu;
+//-----------------------
+function dropMenu(){
+    alert("You rang?");
+}
